054d525f41b0635a6754c51a9ec61f1c0dc189c8
alex_ <alex_@DESKTOP-CBITOJH>
2025-12-02 17:02:31 -0300

    .

---
+++ talenti-pro/src/main/java/talenti/pro/controller/PerguntaController.java
@@ -0,0 +1,97 @@
+package talenti.pro.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.primefaces.model.LazyDataModel;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import talenti.pro.model.Competencia;
+import talenti.pro.model.Pergunta;
+import talenti.pro.repository.GenericLazyDataModel;
+import talenti.pro.service.CompetenciaService;
+import talenti.pro.service.PerguntaService;
+
+@Named(value = "perguntaController")
+@ViewScoped
+public class PerguntaController extends ManagedBeanController {
+
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private PerguntaService perguntaService;
+	
+	@Inject
+	private CompetenciaService competenciaService;
+	
+	private Pergunta pergunta = new Pergunta();
+	private String filtroNome;
+
+	private GenericLazyDataModel<Pergunta> perguntas;
+	private List<Competencia> competencias;
+
+	@PostConstruct
+	public void init() {
+		perguntas = new GenericLazyDataModel<Pergunta>(perguntaService);
+		competencias = competenciaService.listarTodos();
+	}
+
+	public void salvar() {
+		perguntaService.salvar(pergunta);
+		novo();
+		infoSucesso();
+	}
+
+	public void novo() {
+		pergunta = new Pergunta();
+	}
+
+	public void excluir(Pergunta p) {
+		perguntaService.excluir(p.getId());
+		infoSucesso();
+	}
+
+	public void editar(Pergunta p) {
+		pergunta = perguntaService.buscarPorId(p.getId());
+	}
+
+	public void pesquisar() {
+		Map<String, Object> filtros = new HashMap<String, Object>();
+
+		if (filtroNome != null && !filtroNome.isEmpty()) {
+			filtros.put("nome", filtroNome);
+		}
+
+		perguntas.setFixedFilters(filtros);
+	}
+	
+	
+	public List<Competencia> getCompetencias() {
+		return competencias;
+	}
+
+	public Pergunta getPergunta() {
+		return pergunta;
+	}
+
+	public void setPergunta(Pergunta pergunta) {
+		this.pergunta = pergunta;
+	}
+
+	public LazyDataModel<Pergunta> getPerguntas() {
+		return perguntas;
+	}
+
+	public String getFiltroNome() {
+		return filtroNome;
+	}
+
+	public void setFiltroNome(String filtroNome) {
+		this.filtroNome = filtroNome;
+	}
+
+}
---
+++ talenti-pro/src/main/java/talenti/pro/model/Pergunta.java
@@ -0,0 +1,63 @@
+package talenti.pro.model;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode
+@Entity
+@Table(name = "pergunta")
+@SequenceGenerator(name = "seq_pergunta", sequenceName = "seq_pergunta", allocationSize = 1, initialValue = 1)
+public class Pergunta implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_pergunta")
+	private Long id;
+
+	@Column(nullable = false)
+	private String descricao;
+
+	@ManyToOne(optional = false, fetch = FetchType.EAGER)
+	@JoinColumn(name = "competencia_id", nullable = false, foreignKey = @ForeignKey(name = "competencia_fk"))
+	private Competencia competencia;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Competencia getCompetencia() {
+		return competencia;
+	}
+
+	public void setCompetencia(Competencia competencia) {
+		this.competencia = competencia;
+	}
+
+}
