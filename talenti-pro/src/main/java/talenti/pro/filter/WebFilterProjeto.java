c135d0b661c4691150f94ab0be1eddd0b931252f
alex_ <alex_@DESKTOP-CBITOJH>
2025-12-07 20:48:34 -0300

    m

--- talenti-pro/src/main/java/talenti/pro/filter/WebFilterProjeto.java
+++ talenti-pro/src/main/java/talenti/pro/filter/WebFilterProjeto.java
@@ -9,6 +9,8 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @WebFilter("/*")
 public class WebFilterProjeto implements Filter {
@@ -20,7 +22,10 @@ public class WebFilterProjeto implements Filter {
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-
+		
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+		
 		chain.doFilter(request, response);
 	}
 
