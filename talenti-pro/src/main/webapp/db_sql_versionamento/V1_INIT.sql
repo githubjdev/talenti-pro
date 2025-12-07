fd27f6579d13d576fc0796f41f92cf173c63fe2f
alex_ <alex_@DESKTOP-CBITOJH>
2025-12-07 14:40:09 -0300

    Configurar o Elytron no WildFly para autenticação de usuário

--- talenti-pro/src/main/webapp/db_sql_versionamento/V1_INIT.sql
+++ talenti-pro/src/main/webapp/db_sql_versionamento/V1_INIT.sql
@@ -2,7 +2,8 @@
 CREATE OR REPLACE VIEW vw_usuario_roles AS
 SELECT
     u.login       AS username,
-    a.acesso      AS role_name
-FROM usuario u
-JOIN usuario_acesso ua ON ua.usuario_id = u.id
-JOIN acesso a          ON a.id = ua.acesso_id;
+    a.acesso      AS role_name,
+	u.senha       AS senha
+FROM aut_usuario u
+JOIN aut_usuario_acesso ua ON ua.usuario_id = u.id
+JOIN aut_acesso a          ON a.id = ua.acesso_id;
