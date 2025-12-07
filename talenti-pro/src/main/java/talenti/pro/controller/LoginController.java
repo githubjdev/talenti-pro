59c9ad5b50fe31e8be978e3a171a546ac89546e0
alex_ <alex_@DESKTOP-CBITOJH>
2025-12-07 20:16:00 -0300

    dd

--- talenti-pro/src/main/java/talenti/pro/controller/LoginController.java
+++ talenti-pro/src/main/java/talenti/pro/controller/LoginController.java
@@ -1,5 +1,7 @@
 package talenti.pro.controller;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
+
 import jakarta.faces.context.FacesContext;
 import jakarta.faces.view.ViewScoped;
 import jakarta.inject.Named;
@@ -14,27 +16,30 @@ public class LoginController extends ManagedBeanController {
 	private String login;
 	private String senha;
 
-	  public String doLogin() {
-	        try {
-	            HttpServletRequest req = 
-	                (HttpServletRequest) FacesContext.getCurrentInstance()
-	                  .getExternalContext().getRequest();
+	public String doLogin() {
+		try {
+			HttpServletRequest req = (HttpServletRequest) 
+								 FacesContext.getCurrentInstance()
+								.getExternalContext()
+								.getRequest();
 
-	            req.login(login, senha);
+			 req.login(login, senha);
 
-	            return "/pages/index.xhtml?faces-redirect=true";
+			return "/pages/index.xhtml?faces-redirect=true";
 
-	        } catch (Exception e) {
-	        	  e.printStackTrace();
-	            erro("Login inválido");
-	            return null;
-	        }
-	    }
+		} catch (Exception e) {
+			erro("Login Inválido: " + ExceptionUtils.getRootCauseMessage(e));
+			return null;
+		}
+	}
 
 	public void logout() throws Exception {
-		getRequest().logout();
-		getFacesContext().getExternalContext().invalidateSession();
-		getFacesContext().getExternalContext().redirect("../publico/login.xhtml");
+
+		HttpServletRequest request = getRequest();
+		FacesContext context = getFacesContext();
+		request.logout();
+		context.getExternalContext().invalidateSession();
+		context.getExternalContext().redirect(request.getContextPath() + "/publico/login.xhtml?faces-redirect=true");
 	}
 
 	@Override
